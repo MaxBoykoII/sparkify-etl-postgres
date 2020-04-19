55c9d904cf18958b2855ce7a7ad49272c8a6b4ce
Max Boyko <maxboykomax@gmail.com>
2020-04-18 18:35:00 -0700

    feat: Stub out each of the tables

--- sql_queries.py
+++ sql_queries.py
@@ -1,52 +1,69 @@
 # DROP TABLES
 
-songplay_table_drop = ""
-user_table_drop = ""
-song_table_drop = ""
-artist_table_drop = ""
-time_table_drop = ""
+songplay_table_drop = "DROP TABLE IF EXISTS songplays;"
+user_table_drop = "DROP TABLE IF EXISTS users"
+song_table_drop = "DROP TABLE IF EXISTS songs"
+artist_table_drop = "DROP TABLE IF EXISTS artists"
+time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
-""")
+songplay_table_create = """
+CREATE TABLE songplays (songplay_id int)
+"""
 
-user_table_create = ("""
-""")
+user_table_create = """
+CREATE TABLE users (user_id int)
+"""
 
-song_table_create = ("""
-""")
+song_table_create = """
+CREATE TABLE songs (song_id int)
+"""
 
-artist_table_create = ("""
-""")
+artist_table_create = """
+CREATE TABLE artist (artist_id int)
+"""
 
-time_table_create = ("""
-""")
+time_table_create = """
+CREATE TABLE time (time_id int)
+"""
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
-""")
+songplay_table_insert = """
+"""
 
-user_table_insert = ("""
-""")
+user_table_insert = """
+"""
 
-song_table_insert = ("""
-""")
+song_table_insert = """
+"""
 
-artist_table_insert = ("""
-""")
+artist_table_insert = """
+"""
 
 
-time_table_insert = ("""
-""")
+time_table_insert = """
+"""
 
 # FIND SONGS
 
-song_select = ("""
-""")
+song_select = """
+"""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create,
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop,
+]
