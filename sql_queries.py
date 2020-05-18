0a3765b59ab85e72026cfb00d11b698162cf2a0b
Max Boyko <maxboykomax@gmail.com>
2020-05-17 19:21:56 -0700

    feat: Complete steps in etl.ipynb and update table schema

--- sql_queries.py
+++ sql_queries.py
@@ -9,19 +9,19 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 # CREATE TABLES
 
 songplay_table_create = """
-CREATE TABLE songplays (songplay_id int,
-                        start_time int, 
-                        user_id int,
+CREATE TABLE songplays (songplay_id serial primary key,
+                        start_time timestamp references time(start_time), 
+                        user_id int references users(user_id),
                         level varchar,
-                        song_id int,
-                        artist_id int,
+                        song_id varchar references songs(song_id),
+                        artist_id varchar references artists(artist_id),
                         session_id int,
                         location varchar,
                         user_agent varchar)
 """
 
 user_table_create = """
-CREATE TABLE users (user_id int, 
+CREATE TABLE users (user_id int primary key, 
                     first_name varchar, 
                     last_name varchar, 
                     gender varchar, 
@@ -29,7 +29,7 @@ CREATE TABLE users (user_id int,
 """
 
 song_table_create = """
-CREATE TABLE songs (song_id varchar,
+CREATE TABLE songs (song_id varchar primary key,
                     title varchar,
                     artist_id varchar,
                     year int,
@@ -37,7 +37,7 @@ CREATE TABLE songs (song_id varchar,
 """
 
 artist_table_create = """
-CREATE TABLE artists (artist_id varchar,
+CREATE TABLE artists (artist_id varchar primary key,
                       name varchar,
                       location varchar,
                       latitude float,
@@ -45,44 +45,72 @@ CREATE TABLE artists (artist_id varchar,
 """
 
 time_table_create = """
-CREATE TABLE time (time_id int)
+CREATE TABLE time (start_time timestamp primary key,
+                   hour int,
+                   day int,
+                   week int,
+                   month int,
+                   year int,
+                   weekday int)
 """
 
 # INSERT RECORDS
 
 songplay_table_insert = """
+INSERT INTO songplays (start_time, 
+                       user_id, 
+                       level, 
+                       song_id, 
+                       artist_id,
+                       session_id,
+                       location,
+                       user_agent)
+VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
 """
 
 user_table_insert = """
+INSERT INTO users (user_id, first_name, last_name, gender, level)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (user_id) DO NOTHING
 """
 
 song_table_insert = """
 INSERT INTO songs (song_id, title, artist_id, year, duration)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (song_id) DO NOTHING
 """
 
 artist_table_insert = """
 INSERT INTO artists (artist_id, name, location, longitude, latitude)
 VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (artist_id) DO NOTHING
 """
 
 
 time_table_insert = """
+INSERT INTO time (start_time, hour, day, week, month, year, weekday)
+VALUES (%s, %s, %s, %s, %s, %s, %s)
+ON CONFLICT (start_time) DO NOTHING
 """
 
 # FIND SONGS
 
 song_select = """
+SELECT songs.song_id, songs.artist_id FROM songs
+JOIN artists on songs.artist_id = artists.artist_id
+WHERE songs.title = %s
+AND artists.name = %s
+AND songs.duration = %s
 """
 
 # QUERY LISTS
 
 create_table_queries = [
-    songplay_table_create,
     user_table_create,
     song_table_create,
     artist_table_create,
     time_table_create,
+    songplay_table_create,
 ]
 drop_table_queries = [
     songplay_table_drop,
